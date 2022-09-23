df17cae1fdd23aaf50db824510eb6d28d0b78165
Idrissa Dembélé <iddembele206@gmail.com>
2022-09-23 13:51:59 +0000

    creation de sidebarComponent

--- ERPWeb/src/app/app.module.ts
+++ ERPWeb/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
